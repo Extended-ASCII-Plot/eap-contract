1293703f84fa740a4fabfe03e0fa833278a9a138
Found Pan Tiger <renzholy@gmail.com>
2021-10-05 00:36:14 +0800

    tokenURI

--- contracts/ExtendedAsciiPlot.sol
+++ contracts/ExtendedAsciiPlot.sol
@@ -7,7 +7,7 @@ import "./ERC721Preset.sol";
 contract ExtendedAsciiPlot is ERC721Preset {
     uint256 private mintFee = 0.001 ether;
 
-    constructor() ERC721Preset("Extended ASCII Plot", "EAP", "") {}
+    constructor() ERC721Preset("Extended ASCII Plot", "EAP", "/") {}
 
     function setMintFee(uint256 _fee) external onlyOwner {
         mintFee = _fee;
@@ -16,5 +16,9 @@ contract ExtendedAsciiPlot is ERC721Preset {
     function mint(address to, uint256 tokenId) public payable {
         require(msg.value >= mintFee, "Not enough mint fee");
         _safeMint(to, tokenId);
+    }
+
+    function tokenURI(uint256 _tokenId) override public view returns (string memory) {
+        return string(abi.encodePacked(_baseURI(), Strings.toString(_tokenId)));
     }
 }
