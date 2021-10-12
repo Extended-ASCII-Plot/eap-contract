452cea7b557a9404fc110e5a5acc591c832599ab
Found Pan Tiger <renzholy@gmail.com>
2021-10-12 20:05:52 +0800

    update

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,6 +1,13 @@
 import { run, ethers, network } from "hardhat";
 
+const networks = ["mainnet", "rinkeby", "polygon", "mumbai"];
+
 async function main() {
+  if (!networks.includes(network.name)) {
+    console.error(network.name, "not supported.");
+    return;
+  }
+
   await run("compile");
 
   const [deployer] = await ethers.getSigners();
@@ -10,14 +17,19 @@ async function main() {
     console.log("Account balance:", (await deployer.getBalance()).toString());
   }
 
-  const Contract = await ethers.getContractFactory("ExtendedAsciiPlot");
+  const Contract = await ethers.getContractFactory(
+    network.name === "polygon" || network.name === "mumbai"
+      ? "ExtendedAsciiPlotPloygon"
+      : "ExtendedAsciiPlot"
+  );
   const contract = await Contract.deploy(
     // OpenSea proxy registry addresses
-    network.name === "rinkeby"
-      ? "0xf57b2c51ded3a29e6891aba85459d600256cf317"
-      : network.name === "polygon"
-      ? "0x58807baD0B376efc12F5AD86aAc70E78ed67deaE"
-      : "0xa5409ec958c83c3f309868babaca7c86dcb077c1"
+    {
+      mainnet: "0xa5409ec958c83c3f309868babaca7c86dcb077c1",
+      rinkeby: "0xf57b2c51ded3a29e6891aba85459d600256cf317",
+      polygon: "0x58807baD0B376efc12F5AD86aAc70E78ed67deaE",
+      mumbai: "",
+    }[network.name]
   );
 
   console.log("Contract address:", contract.address);
