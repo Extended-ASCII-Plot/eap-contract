a6fd97f79fb433d7e9b02e837acce8631e2b5adb
Found Pan Tiger <renzholy@gmail.com>
2022-03-15 19:02:31 +0800

    remove ERC721Enumerable

--- contracts/ExtendedAsciiPlot.sol
+++ contracts/ExtendedAsciiPlot.sol
@@ -5,17 +5,23 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 import "./SVG.sol";
 
 error QueryNonexistentToken();
 error MintUnderPrice();
 
-contract ExtendedAsciiPlot is Ownable, ERC721Enumerable {
+contract ExtendedAsciiPlot is Ownable, ERC721 {
     using Strings for uint256;
 
     uint256 private mintFee = 0 ether;
+
+    // The tokenId of the next token to be minted.
+    uint256 private _currentIndex;
+
+    // Mapping from tokenId to token index
+    mapping(uint256 => uint256) private _tokensIndex;
 
     constructor() ERC721("Extended ASCII Plot", "EAP") {}
 
@@ -23,6 +29,8 @@ contract ExtendedAsciiPlot is Ownable, ERC721Enumerable {
         if (msg.value < mintFee) revert MintUnderPrice();
 
         _safeMint(to, tokenId);
+
+        _tokensIndex[tokenId] = _currentIndex++;
     }
 
     function tokenURI(uint256 tokenId)
@@ -40,7 +48,7 @@ contract ExtendedAsciiPlot is Ownable, ERC721Enumerable {
                     Base64.encode(
                         abi.encodePacked(
                             '{"name": "EAP #',
-                            tokenId.toString(),
+                            _tokensIndex[tokenId].toString(),
                             '", "description": "Extended ASCII Plot (EAP) is user-generated 256bit textmode art fully stored on chain.", "image": "data:image/svg+xml;base64,',
                             Base64.encode(SVG.svg(tokenId)),
                             '"}'
@@ -48,6 +56,10 @@ contract ExtendedAsciiPlot is Ownable, ERC721Enumerable {
                     )
                 )
             );
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _currentIndex;
     }
 
     function setMintFee(uint256 _fee) external onlyOwner {
