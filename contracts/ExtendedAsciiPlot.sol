62d3a901c58f8b0cf1d74bea81a061c74b95e7d2
Found Pan Tiger <renzholy@gmail.com>
2022-03-15 18:55:26 +0800

    Revert "erc721a"
    This reverts commit 00f32be7eb3584f78676defe2a10ab23e9e06870.

--- contracts/ExtendedAsciiPlot.sol
+++ contracts/ExtendedAsciiPlot.sol
@@ -5,30 +5,24 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
-import "erc721a/contracts/ERC721A.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
 import "./SVG.sol";
 
 error QueryNonexistentToken();
 error MintUnderPrice();
-error ValueAlreadyMinted();
 
-contract ExtendedAsciiPlot is Ownable, ERC721A {
+contract ExtendedAsciiPlot is Ownable, ERC721Enumerable {
     using Strings for uint256;
 
     uint256 private mintFee = 0 ether;
 
-    constructor() ERC721A("Extended ASCII Plot", "EAP") {}
+    constructor() ERC721("Extended ASCII Plot", "EAP") {}
 
-    mapping(uint256 => uint256) public tokens;
-
-    function mint(address to, uint256 value) public payable {
+    function mint(address to, uint256 tokenId) public payable {
         if (msg.value < mintFee) revert MintUnderPrice();
-        if (tokens[totalSupply()] > 0) revert ValueAlreadyMinted();
 
-        _safeMint(to, 1);
-
-        tokens[totalSupply()] = value;
+        _safeMint(to, tokenId);
     }
 
     function tokenURI(uint256 tokenId)
@@ -39,23 +33,21 @@ contract ExtendedAsciiPlot is Ownable, ERC721A {
     {
         if (!_exists(tokenId)) revert QueryNonexistentToken();
 
-        unchecked {
-            return
-                string(
-                    abi.encodePacked(
-                        "data:application/json;base64,",
-                        Base64.encode(
-                            abi.encodePacked(
-                                '{"name": "EAP #',
-                                tokenId.toString(),
-                                '", "description": "Extended ASCII Plot (EAP) is user created 256bit textmode art fully stored on chain.", "image": "data:image/svg+xml;base64,',
-                                Base64.encode(SVG.svg(tokens[tokenId])),
-                                '"}'
-                            )
+        return
+            string(
+                abi.encodePacked(
+                    "data:application/json;base64,",
+                    Base64.encode(
+                        abi.encodePacked(
+                            '{"name": "EAP #',
+                            tokenId.toString(),
+                            '", "description": "Extended ASCII Plot (EAP) is user created 256bit textmode art fully stored on chain.", "image": "data:image/svg+xml;base64,',
+                            Base64.encode(SVG.svg(tokenId)),
+                            '"}'
                         )
                     )
-                );
-        }
+                )
+            );
     }
 
     function setMintFee(uint256 _fee) external onlyOwner {
@@ -63,6 +55,7 @@ contract ExtendedAsciiPlot is Ownable, ERC721A {
     }
 
     function withdraw() external onlyOwner {
-        payable(owner()).transfer(address(this).balance);
+        address payable _owner = payable(owner());
+        _owner.transfer(address(this).balance);
     }
 }
