7ce45f2a51c988efd38a8f55ac8cf7306acd1e18
Found Pan Tiger <renzholy@gmail.com>
2022-03-17 22:03:42 +0800

    Attribute

---
+++ contracts/Attribute.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+library Attribute {
+    function distinctCountOfChars(uint256 value) public pure returns (uint256) {
+        unchecked {
+            uint256 bits;
+            for (uint256 i = 0; i < 16; i++) {
+                uint8 t = uint8((value & 0xff00) >> 0x8);
+                bits |= 1 << t;
+            }
+            return countSetBits(bits);
+        }
+    }
+
+    function countSetBits(uint256 value) public pure returns (uint256) {
+        if (value == 0) return 0;
+        else return countSetBits(value & (value - 1)) + 1;
+    }
+}
---
+++ test/Attribute.test.ts
@@ -0,0 +1,29 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+import { Attribute } from "../typechain-types/Attribute";
+
+describe("Attribute", function () {
+  let signers: SignerWithAddress[];
+  let library: Attribute;
+
+  this.beforeAll(async () => {
+    const Library = await ethers.getContractFactory("Attribute");
+    library = (await Library.deploy()) as Attribute;
+    await library.deployed();
+    signers = await ethers.getSigners();
+  });
+
+  it("countSetBits", async () => {
+    expect(await library.countSetBits(0b0)).to.eq(ethers.BigNumber.from(0));
+    expect(await library.countSetBits(0b111100001001)).to.eq(
+      ethers.BigNumber.from(6)
+    );
+    expect(
+      await library.countSetBits(
+        ethers.BigNumber.from("0xffffffffffffffffffffffffffffffff")
+      )
+    ).to.eq(ethers.BigNumber.from(128));
+  });
+});
